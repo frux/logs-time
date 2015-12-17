bc87bf695256ad1dca367b75286791bbc9dafce4
Vladimir Kudinov <frux@yandex-team.ru>
2015-12-17 14:29:16 +0500

    First scetch

---
+++ index.js
@@ -0,0 +1,20 @@
+var StreamTransformer = require('stream-transformer');
+var dateFormat = require('dateformat');
+
+module.exports = function(pattern){
+	var oldStdout = process.stdout,
+		oldStderr = process.stderr;
+
+	pattern = pattern || 'yyyy-mm-ddTHH:MM:ssZ ';
+
+	delete process.stdout;
+	delete process.stderr;
+
+	process.stdout = new StreamTransformer(oldStdout, function(data){
+		return dateFormat(pattern) + data;
+	});
+
+	process.stderr = new StreamTransformer(oldStderr, function(data){
+		return dateFormat(pattern) + data;
+	});
+};
